803c31f38660f1d5ca0512659e97e82d668d3876
DerekHan <donglak.han@gmail.com>
2023-08-20 09:47:43 -0500

    fixing Image on sizes

--- components/cards/GoriCard.tsx
+++ components/cards/GoriCard.tsx
@@ -50,6 +50,7 @@ export default function GoriCard({
                 src={author.image}
                 alt="Profile Image"
                 fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 className="cursor-pointer rounded-full"
               />
             </Link>
--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -27,6 +27,7 @@ export default function ProfileHeader({
               src={imgUrl}
               alt="Profile Image"
               fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               className="rounded-full object-cover shadow-2xl"
             />
           </div>
