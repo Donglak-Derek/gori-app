33a352f0f96dab5ee4e7a4358d69dd082baf7d28
DerekHan <donglak.han@gmail.com>
2023-08-23 10:35:55 -0500

    titles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,7 +25,9 @@ export default async function Home({
 
   return (
     <>
-      <h1 className="head-text text-left">Home</h1>
+      <h1 className="head-text text-left">
+        What are you thinking? (Use Korean)
+      </h1>
 
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
